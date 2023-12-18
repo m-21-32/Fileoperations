3530199ad70d09092ee01d1f5ff0b723ac56cd16
m-21-32 <123524203+m-21-32@users.noreply.github.com>
2023-12-18 17:24:36 -0500

    Implement Mongoose, CRUD operations, and validation for workouts, nutrition, and goals

--- server.js
+++ server.js
@@ -1,28 +1,76 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const mongoose = require('mongoose');
+const { body, validationResult } = require('express-validator');
 const path = require('path');
- 
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/api/users') {
-    // Read user data from a file using fs and path modules
-    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(data);
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+
+const app = express();
+app.use(express.json());
+
+// MongoDB and Mongoose setup
+mongoose.connect('mongodb://localhost:27017/fitnessTracker', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Mongoose Schemas
+const workoutSchema = new mongoose.Schema({
+  type: String,
+  duration: Number,
+  calories_burned: Number
+});
+
+const nutritionSchema = new mongoose.Schema({
+  meal: String,
+  calories: Number,
+  protein: Number
+});
+
+const goalSchema = new mongoose.Schema({
+  goal_type: String,
+  target: Number
+});
+
+// Mongoose Models
+const Workout = mongoose.model('Workout', workoutSchema);
+const Nutrition = mongoose.model('Nutrition', nutritionSchema);
+const Goal = mongoose.model('Goal', goalSchema);
+
+// CRUD Routes for workouts
+app.get('/workouts', async (req, res) => {
+  try {
+    const workouts = await Workout.find();
+    res.json(workouts);
+  } catch (error) {
+    console.error('Error fetching workouts:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
- 
-// Start the server on port 3000
-server.listen(3000, () => {
-  console.log('Server running on <http://localhost:3000/>');
+
+app.post('/workouts', [
+  body('type').isLength({ min: 1 }),
+  body('duration').isNumeric(),
+  body('calories_burned').isNumeric()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const newWorkout = new Workout(req.body);
+    await newWorkout.save();
+    res.json(newWorkout);
+  } catch (error) {
+    console.error('Error creating workout:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
- 
+
+
+
+// Serve static files from the 'public' directory
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+// Start the server on port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
